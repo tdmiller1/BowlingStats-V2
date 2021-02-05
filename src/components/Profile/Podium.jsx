49ed98c0c907ec44d1e6267b6d82bc715a8c84b6
Tucker Miller <tuckerdanielmiller@gmail.com>
2021-02-04 23:58:58 -0500

    Add correct data to podium

--- src/components/Profile/Podium.jsx
+++ src/components/Profile/Podium.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { getPublicPlayerData } from '../../utils/gameApi';
 
 const Podium = () => {
+  const { getAccessTokenSilently, user } = useAuth0();
+  const [playerData, setPlayerData] = useState({});
+
+  useEffect(() => {
+    getAccessTokenSilently().then((token) => {
+      getPublicPlayerData(user.sub, token).then(result => {
+        setPlayerData(result.response?.data)
+      })
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className='flex justify-center pt-4 mt-4'>
       <div className='flex items-end'>
-        <div className='Profile-second'><div className="Profile-PodiumResult">2nd</div></div>
-        <div className='Profile-first'><div className="Profile-PodiumResult">1st</div></div>
-        <div className='Profile-third'><div className="Profile-PodiumResult">3rd</div></div>
+        <div className='Profile-second'><div className="Profile-PodiumResult">{playerData.average}</div></div>
+        <div className='Profile-first'><div className="Profile-PodiumResult">{playerData.highScore}</div></div>
+        <div className='Profile-third'><div className="Profile-PodiumResult">{playerData.lowScore}</div></div>
       </div>
     </div>
   );
 }
+
 export default Podium;
