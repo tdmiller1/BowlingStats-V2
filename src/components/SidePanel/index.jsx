a7b51ad7ab8c4c19f062627746a23e33de4f100f
Tucker Miller <tuckerdanielmiller@gmail.com>
2021-09-22 19:41:39 -0500

    Fix some issues

--- src/components/SidePanel/index.jsx
+++ src/components/SidePanel/index.jsx
@@ -1,45 +1,43 @@
-import React, {useState} from 'react';
-import DayPicker from 'react-day-picker';
-import {Button,
-  TextField,
-  } from '@material-ui/core'
-import { useAuth0 } from '@auth0/auth0-react';
+import React, { useState } from "react";
+import DayPicker from "react-day-picker";
+import { Button, TextField } from "@material-ui/core";
+import { useAuth0 } from "@auth0/auth0-react";
 
-import './SidePanel.scss';
-import 'react-day-picker/lib/style.css';
-import { addGame } from '../../utils/gameApi';
+import "./SidePanel.scss";
+import "react-day-picker/lib/style.css";
+import { addGame } from "../../utils/gameApi";
 
-const SidePanel = ({theme, addGameCallback}) => {
+const SidePanel = ({ theme, addGameCallback }) => {
   const [gameScore, setGameScore] = useState(null);
-  const [selectedDay, setSelectedDay] = useState(null);
+  const [selectedDay, setSelectedDay] = useState(new Date());
   const [errorMessage, setErrorMessage] = useState(null);
   const { user, getAccessTokenSilently } = useAuth0();
 
   const handleDayClick = (day, { selected }) => {
     if (day < new Date()) {
-      setSelectedDay(selected ? undefined : day)
+      setSelectedDay(selected ? undefined : day);
     }
-  }
+  };
 
   const resetGameInfo = () => {
     setGameScore(null);
     setErrorMessage(null);
     addGameCallback();
-  }
+  };
 
   const handleSubmit = () => {
-    if(gameScore === null || gameScore === 0){
+    if (gameScore === null || gameScore === 0) {
       setErrorMessage("Enter Game Score");
-    }else if(selectedDay === null || selectedDay === undefined){
+    } else if (selectedDay === null || selectedDay === undefined) {
       setErrorMessage("Enter Date of Game");
-    }else{
+    } else {
       getAccessTokenSilently().then((token) => {
-        addGame(user.sub, gameScore, selectedDay, token).then(result => {
+        addGame(user.sub, gameScore, selectedDay, token).then((result) => {
           result.error ? setErrorMessage(result.error) : resetGameInfo();
-        })
+        });
       });
     }
-  }
+  };
 
   return (
     <div className="SidePanel">
@@ -47,15 +45,16 @@ const SidePanel = ({theme, addGameCallback}) => {
         onSubmit={(e) => {
           e.preventDefault();
           handleSubmit();
-        }}>
+        }}
+      >
         <TextField
           className="SidePanel-input"
           InputProps={{ inputProps: { min: 1, max: 300 } }}
           type="number"
           variant="outlined"
-          value={gameScore || ''}
+          value={gameScore || ""}
           placeholder="Score"
-          onChange={e => setGameScore(parseInt(e.target.value,10) || 0)}
+          onChange={(e) => setGameScore(parseInt(e.target.value, 10) || 0)}
         />
         <DayPicker
           className="SidePanel-DayPicker"
@@ -69,11 +68,18 @@ const SidePanel = ({theme, addGameCallback}) => {
             },
           ]}
         />
-        <Button className="SidePanel-SubmitButton iconButton" variant="contained" color="primary" type="submit">Add Game</Button>
+        <Button
+          className="SidePanel-SubmitButton iconButton"
+          variant="contained"
+          color="primary"
+          type="submit"
+        >
+          Add Game
+        </Button>
         {errorMessage && <h3>{errorMessage}</h3>}
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default SidePanel;
